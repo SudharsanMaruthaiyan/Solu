c7ce07874a6a5455575204546438cb5d2dc3c601
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2024-08-23 22:13:06 +0530

    changes done

--- src/Components/CompanyCard/CompanyCard.jsx
+++ src/Components/CompanyCard/CompanyCard.jsx
@@ -7,7 +7,7 @@ const CompanyCard = () => {
       <div className="mb-10 z-10 group rounded-2xl bg-white " id="cardbg">
         <div className="rounded-2xl text-center px-6 py-10  z-0" id="bg">
           <div className=" flex justify-center">
-            <Boxes className=" group-hover:stroke-white stroke-[#ff3d01] h-10 w-[60px]"/>
+            <Boxes className=" group-hover:stroke-white stroke-[#1176F0] h-10 w-[60px]"/>
           </div>
           <h2 className=" font-[poppions-r] font-bold text-[22px] pt-9 pb-5 group-hover:text-white text-center">Web Development</h2>
           <p className=" font-[firesans-r] text-[16px] text-[#6e6d6d] font-medium p-0 group-hover:text-white text-center">Marketing repurpose success in professions whereas in services sapien maximus design.</p>
@@ -16,7 +16,7 @@ const CompanyCard = () => {
       <div className="mb-10 z-10 group rounded-2xl bg-white " id="cardbg">
         <div className="rounded-2xl text-center px-6 py-10  z-0" id="bg">
           <div className=" flex justify-center">
-            <Blocks className=" group-hover:stroke-white stroke-[#ff3d01] h-10 w-[60px]"/>
+            <Blocks className=" group-hover:stroke-white stroke-[#1176F0] h-10 w-[60px]"/>
           </div>
           <h2 className=" font-[poppions-r] font-bold text-[22px] pt-9 pb-5 group-hover:text-white text-center">IT Management</h2>
           <p className=" font-[firesans-r] text-[16px] text-[#6e6d6d] font-medium p-0 group-hover:text-white text-center">Marketing repurpose success in professions whereas in services sapien maximus design.</p>
@@ -25,7 +25,7 @@ const CompanyCard = () => {
       <div className="mb-10 z-10 group rounded-2xl bg-white " id="cardbg">
         <div className="rounded-2xl text-center px-6 py-10  z-0" id="bg">
           <div className=" flex justify-center">
-            <BadgeIndianRupee className=" group-hover:stroke-white stroke-[#ff3d01] h-10 w-[60px]"/>
+            <BadgeIndianRupee className=" group-hover:stroke-white stroke-[#1176F0] h-10 w-[60px]"/>
           </div>
           <h2 className=" font-[poppions-r] font-bold text-[22px] pt-9 pb-5 group-hover:text-white text-center">Digital Marketing</h2>
           <p className=" font-[firesans-r] text-[16px] text-[#6e6d6d] font-medium p-0 group-hover:text-white text-center">Marketing repurpose success in professions whereas in services sapien maximus design.</p>
@@ -34,7 +34,7 @@ const CompanyCard = () => {
       <div className="mb-10 z-10 group rounded-2xl bg-white " id="cardbg">
         <div className="rounded-2xl text-center px-6 py-10  z-0" id="bg">
           <div className=" flex justify-center">
-            <TabletSmartphone className=" group-hover:stroke-white stroke-[#ff3d01] h-10 w-[60px]"/>
+            <TabletSmartphone className=" group-hover:stroke-white stroke-[#1176F0] h-10 w-[60px]"/>
           </div>
           <h2 className=" font-[poppions-r] font-bold text-[22px] pt-9 pb-5 group-hover:text-white text-center">App Development</h2>
           <p className=" font-[firesans-r] text-[16px] text-[#6e6d6d] font-medium p-0 group-hover:text-white text-center">Marketing repurpose success in professions whereas in services sapien maximus design.</p>
--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -4,17 +4,18 @@ import CompanyCard from "../CompanyCard/CompanyCard"
 const HeroSection = () => {
   return (
     <>
-      <div className=" 2xl:container shadow-lg mx-auto bg-[#F4F4F4]">
+    {/* bg-[#7bb4fa] */}
+      <div className=" 2xl:container shadow-lg mx-auto ">
         <div className=" lg:w-[85%] mx-auto w-[90%]">
           <div className=" flex flex-col items-center justify-center py-10">
             <div className=" relative">
-              <h1 className=" text-[#FF3D00] font-[poppions-r] text-[16px] font-bold bg-[#F4EEEC] w-fit py-2 px-7  border-2 rounded-full mb-5 ">SOLUTEK COMPANY</h1>
-              <div className=" absolute top-4 -left-5 h-[7px] w-8 rounded-full bg-[#FF3D00]">
+              <h1 className=" text-[#1176F0] font-[poppions-r] text-[16px] font-bold bg-[#5ba5ff6e] w-fit py-2 px-7 border-2 rounded-full mb-5 ">CODE GALLATA</h1>
+              <div className=" absolute top-5 -left-5 h-[6px] w-8 rounded-full bg-[#1176F0]">
 
               </div>
             </div>
             <h1 className=" font-[poppions-b] text-[42px] font-black text-center text-wrap">How Professional IT Services <br />
-            Can Drive <span className=" text-[#FF3C00]"> Success.</span></h1>
+            Can Drive <span className=" text-[#1176F0]"> Success.</span></h1>
           </div>
           <div className=" ">
             <CompanyCard/>
